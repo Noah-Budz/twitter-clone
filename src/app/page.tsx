58c9b52137ede92a84befa73fe654d29da89e873
Noah-Budz <noah.v.budz@gmail.com>
2023-08-14 09:42:35 -0700

    Added main timeline component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,17 @@
-import Link from 'next/link'
-import {BiHomeCircle, BiUser} from 'react-icons/bi'
-import {BsBell, BsBookmark, BsTwitter,BsEnvelope, BsThreeDots} from 'react-icons/bs'
-import {HiOutlineHashtag} from 'react-icons/hi'
+import LeftSidebar from "@/components/LeftSidebar"
+import MainTimeline from "@/components/MainTimeline"
 
-const NAVIGATION_ITEMS = [
-  {
-    title: 'twitter',
-    icon: BsTwitter
-  },
-  {
-    title: 'home',
-    icon: BiHomeCircle
-  },
-  {
-    title: 'Explore',
-    icon: HiOutlineHashtag
-  },
-  {
-    title: 'Notifications',
-    icon: BsBell
-  },
-  {
-    title: 'Messages',
-    icon: BsEnvelope
-  },
-  {
-    title: 'Bookmarks',
-    icon: BsBookmark
-  },
-  {
-    title: 'Profile',
-    icon: BiUser
-  }
-]
-
-
-
-
-
-const page = () => {
+const Home = () => {
   return (
     <div className='w-full h-full flex justify-center items-center relative bg-black'>
       <div className='max-w-screen-xl w-full h-full flex relative'>
         {/* left sidebar for navigation/header */}
-        <section className='fixed w-[275px] flex flex-col items-stretch h-screen space-y-4'> 
-          <div className='flex flex-col items-stretch h-full space-y-4 mt-4'>
-            {NAVIGATION_ITEMS.map((item)=>(
-              <Link 
-              className='hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 
-              rounded-3xl py-2 px-6' 
-              href={'/${item.title.toLowerCase()}'} 
-              key={item.title}>
-                <div>
-                  <item.icon />
-                </div>
-              {item.title !== 'Twitter' && <div>{item.title}</div>}
-              </Link>
-            ))}
-            <button className='rounded-full m-4 bg-primary p-4 text-2xl text-center hover:bg-opacity-70 transition duration-200'>
-              Tweet
-            </button>
-          </div>
-          <button className='rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/10 
-          transition duration-200 w-full justify-between'>
-            <div className='flex items-center space-x-2'>
-              <div className='rounded-full bg-slate-400 w-8 h-8'></div>
-              <div className='text-left text-sm'>
-                <div className='font-semibold'>SPongus</div>
-                <div className=''>@SPongus</div>
-              </div>
-            </div>
-            <div>
-              <BsThreeDots />
-            </div>
-          </button>
-        </section>
-        {/* <main className="">Home timeline</main>
-        <section className="">right section</section> */}
+        <LeftSidebar />
+        <MainTimeline />
+        {/* <section className="">right section</section> */}
       </div>
     </div>
   )
 }
 
-export default page
+export default Home
